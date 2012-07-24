688dc9987615ffb7c5ad122f11866f67b52cfebe
alejodj1984 <alejo_dj1984@hotmail.com>
2012-07-23 21:16:13 -0500

    comenzamos

---
+++ src/com/example/git/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.git;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.support.v4.app.NavUtils;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+
+
+    public void botonabrir(View view) {
+    	Intent a=new Intent (this,actividad2.class);
+    	startActivity(a);
+    	
+    }
+}
---
+++ src/com/example/git/actividad2.java
@@ -0,0 +1,21 @@
+package com.example.git;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.support.v4.app.NavUtils;
+
+public class actividad2 extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.actividad2);
+        
+    }
+
+    
+  
+
+}
